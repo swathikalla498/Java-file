42dd084e860c4a6294743af718a43c29ffee58f0
swathikalla498 <147309053+swathikalla498@users.noreply.github.com>
2023-10-16 22:23:33 +0530

    creating a sleep()
    sleep method with 0mls

---
+++ threads/Sleep.java
@@ -0,0 +1,20 @@
+package threads;
+
+public class Sleep extends Thread{
+	    
+		 public void run(){    
+		  for(int i=1;i<5;i++){   
+		 
+		    try{Thread.sleep(0);}catch(InterruptedException e){System.out.println(e);}    
+		    System.out.println(i);    
+		  }    
+		 }    
+		 public static void main(String args[]){    
+		  Sleep t1=new Sleep();    
+		  Sleep t2=new Sleep();    
+		     
+		  t1.start();    
+		  t2.start();    
+		 }    
+		}    
+
