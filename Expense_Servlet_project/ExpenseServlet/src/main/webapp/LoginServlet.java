9a02aab4b6fe55354fef2cd1f6a11954092680c2
swathikalla498 <147309053+swathikalla498@users.noreply.github.com>
2023-11-19 14:42:42 +0530

    Update LoginServlet.java
    updated loginservlet

--- Expense_Servlet_project/ExpenseServlet/src/main/webapp/LoginServlet.java
+++ Expense_Servlet_project/ExpenseServlet/src/main/webapp/LoginServlet.java
@@ -2,12 +2,20 @@ package expense;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.x.protobuf.MysqlxResultset;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
 
 import jakarta.servlet.RequestDispatcher;
 
@@ -17,23 +25,30 @@ public class LoginServlet extends HttpServlet {
      
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
-		
-		PrintWriter pw=response.getWriter();
-		
-		String s=request.getParameter("user_name");
+		response.setContentType("text/html");
+		try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/daily_spends", "root", "Santu9999@");
+	    String s=request.getParameter("user_name");
 		String p=request.getParameter("password");
+		PreparedStatement ps=con.prepareStatement("select User_name from Users_Details where User_name=? and User_password=?");
+		ps.setString(1, s);
+		ps.setString(2, p);
+	ResultSet rs=ps.executeQuery();
+	 if(rs.next()) {
+		 RequestDispatcher rd=request.getRequestDispatcher("HomePage.html");
+		 rd.forward(request, response);
+	 }else {
+		 RequestDispatcher rd=request.getRequestDispatcher("<a href=Login.html />");
+	 }
 		
-		if(Login.validate(s,p)) {
-			
-			RequestDispatcher r=(RequestDispatcher) request.getRequestDispatcher("Hpomepage.html");
-			r.forward(request, response);
-		}else {
-			PrintWriter out = null;
-			out.println("incorrect user_name or password");
-			RequestDispatcher r=(RequestDispatcher) request.getRequestDispatcher("Login.html");
-			r.include(request, response);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
 	}
 
-}
+	}
+
+
